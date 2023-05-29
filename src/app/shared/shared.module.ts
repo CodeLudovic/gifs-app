c2a199fcc57fe20c8cb1fab75ba318de0a86b229
CodeLudovic <danielospinar@gmail.com>
2023-05-28 22:48:40 -0500

    App Gif's With Image Loader

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { GifsService } from '../gifs/services/gifs.service';
+import { LazyImageComponent } from './components/lazy-image/lazy-image.component';
 
 
 
 @NgModule({
   declarations: [
-    SidebarComponent
+    SidebarComponent,
+    LazyImageComponent
   ],
   imports: [
     CommonModule
@@ -16,7 +18,8 @@ import { GifsService } from '../gifs/services/gifs.service';
 
   ],
   exports:[
-    SidebarComponent
+    SidebarComponent,
+    LazyImageComponent
   ]
 })
 export class SharedModule { }
